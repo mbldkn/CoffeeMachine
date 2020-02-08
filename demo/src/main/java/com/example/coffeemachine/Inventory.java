db9f5bd8788f9b4258e1c6cf6f7a1e9747dda605
mbldkn <melikesardogan@gmail.com>
2020-02-09 02:23:30 +0300

    - Added Inventory.

---
+++ demo/src/main/java/com/example/coffeemachine/Inventory.java
@@ -0,0 +1,15 @@
+package com.example.coffeemachine;
+
+public class Inventory
+{
+    public static int waterAmount = 2000; // ml
+    public static int milkAmount = 1000; // ml
+    public static int cupCount = 200; // Cup Quantity
+    public static int standartCoffeeAmount = 500; // gr
+    public static int hazelnutCoffeeAmount = 500; // gr
+    public static int guatemalaCoffeeAmount = 500; // gr
+    public static int costaRicaCoffeeAmount = 500; // gr
+    public static int colombiaCoffeeAmount = 500; // gr
+    public static int antiguaCoffeeAmount = 500; // gr
+    public static int elSalvadorCoffeeAmount = 500; // gr
+}
