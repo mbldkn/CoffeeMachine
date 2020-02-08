3fd0e0442a7e45459726b9bf3d4097f735304b9d
mbldkn <melikesardogan@gmail.com>
2020-02-09 02:24:32 +0300

    - Added new services to CoffeeMachineController.

--- demo/src/main/java/com/example/coffeemachine/CoffeeMachineController.java
+++ demo/src/main/java/com/example/coffeemachine/CoffeeMachineController.java
@@ -1,14 +1,36 @@
 package com.example.coffeemachine;
 
+import com.example.coffeemachine.enums.CoffeeType;
+import com.example.coffeemachine.enums.CupSize;
+import com.example.coffeemachine.enums.MilkAmount;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CoffeeMachineController
 {
+
+    @Autowired
+    CoffeeService coffeeService;
+
     @GetMapping("/coffee-types")
-    public String CoffeeTypes()
+    public List<CoffeeType> CoffeeTypes()
     {
-        return "Filter";
+        return coffeeService.getCoffeeTypes();
+    }
+
+    @GetMapping("/cup-sizes")
+    public List<CupSize> CupSizes()
+    {
+        return coffeeService.getCupSizes();
+    }
+
+    @GetMapping("/milk-amount")
+    public List<MilkAmount> MilkAmount()
+    {
+        return coffeeService.getMilkAmount();
     }
 }
